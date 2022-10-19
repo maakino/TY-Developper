4bf3b0d9665e42a7c5f10bc6ff7e6502fa1f1748
Maakino <teddyequerre0@gmail.com>
2022-10-19 16:11:19 +0200

    add toggle for header mobile

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const burger = document.querySelector('.js-burger');
+const navsMobile = document.querySelectorAll('.js-toggleNav');
+
+
+function toggleNav(){
+    navsMobile.forEach(function (nav){
+        nav.classList.toggle('hiddenMobile')
+    })
+}
+
+burger.addEventListener('click', () =>{
+    toggleNav();
+})
+
+navsMobile.forEach(nav =>{
+    nav.querySelectorAll('a').forEach((link, index)=>{
+        link.addEventListener('click',()=>{
+        toggleNav();
+        })
+    })
+})
+
+
+
+

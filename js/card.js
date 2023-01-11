29a3735de4a213d5fc4c73b99f687f8fa507a3d0
Maakino <teddyequerre0@gmail.com>
2023-01-11 13:36:48 +0100

    add new version and project

--- js/card.js
+++ js/card.js
@@ -1,14 +1,19 @@
 const gallery = document.querySelector('#gallery');
 portfolio.map((project, index) =>{
     const img = `
+    <div>
+    <div class="card-hover"
+    onclick="createPopup(${index})">
+    <h2>${project.title}</h2>
+    </div>
             <img
               src="${project.img_src}"
               alt="miniature ${project.title}"
               width="414"
               height="219"
               loading="lazy"
-              onclick="createPopup(${index})"
             />
+    </div>
     `;
     gallery.innerHTML+= img;
 
--- js/data.js
+++ js/data.js
@@ -24,5 +24,21 @@ const portfolio = [
         url_maquette:"img/portfolio/tydevelopper.pdf" ,
         img_src: "img/portfolio/Tydevelopper.jpg",
         type: "maquette"
+    },
+    {
+        title: "Best tastes",
+        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Logiciel : Figma ",
+        url_site: null,
+        url_maquette:"img/portfolio/bestTastes.pdf" ,
+        img_src: "img/portfolio/bestTastes.jpg",
+        type: "maquette"
+    },
+    {
+        title: "JCV Consult",
+        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Technologies : React / SASS / Git ",
+        url_site: "https://jcvconsult.re/",
+        url_maquette:null ,
+        img_src: "img/portfolio/jcvConsult.jpg",
+        type: "site"
     }
 ]

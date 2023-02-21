4249c2c40b1575e89e69078f19e9ca3473505a3e
Maakino <teddyequerre0@gmail.com>
2023-02-21 09:11:42 +0100

    add new projet and delete last project

--- js/data.js
+++ js/data.js
@@ -1,37 +1,11 @@
 const portfolio = [
     {
-        title: "Devis power",
-        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Technologies : React / SASS / Git  ",
-        url_site: "https://www.devispower.fr/",
-        url_maquette: null,
-        img_src: "img/portfolio/devisPower.jpg",
+        title: "Maz Pronos",
+        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Technologies : Html / SASS / Wordpress ",
+        url_site: "https://mazpronos.com/",
+        url_maquette:null ,
+        img_src: "img/portfolio/MazPronos.jpg",
         type: "site"
-    },
-
-    {
-        title: "Cuisine créole",
-        desc: "Maquette entrainement débutant <br> Logicièle utiliseé : Figma",
-        url_site: null,
-        url_maquette:"img/portfolio/cuisine-créole.pdf" ,
-        img_src: "img/portfolio/test.jpg",
-        type: "maquette"
-    },
-
-    {
-        title: "Tydevelopper",
-        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Technologies : React / SASS / Git ",
-        url_site: null,
-        url_maquette:"img/portfolio/tydevelopper.pdf" ,
-        img_src: "img/portfolio/Tydevelopper.jpg",
-        type: "maquette"
-    },
-    {
-        title: "Best tastes",
-        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Logiciel : Figma ",
-        url_site: null,
-        url_maquette:"img/portfolio/bestTastes.pdf" ,
-        img_src: "img/portfolio/bestTastes.jpg",
-        type: "maquette"
     },
     {
         title: "JCV Consult",
@@ -39,6 +13,14 @@ const portfolio = [
         url_site: "https://jcvconsult.re/",
         url_maquette:null ,
         img_src: "img/portfolio/jcvConsult.jpg",
+        type: "site"
+    },
+    {
+        title: "Devis power",
+        desc: "Design de la maquette graphique <br> Intégration Front-end <br> Technologies : React / SASS / Git  ",
+        url_site: "https://www.devispower.fr/",
+        url_maquette: null,
+        img_src: "img/portfolio/devisPower.jpg",
         type: "site"
     }
 ]

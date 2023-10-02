56b26ae2cf9a4b3cc057837a1d6a0ed5dc212ea4
Maakino <teddyequerre0@gmail.com>
2023-10-02 11:51:07 +0200

    add header animation

--- js/script.js
+++ js/script.js
@@ -2,27 +2,32 @@ const burger = document.querySelector('.js-burger');
 const navsMobile = document.querySelectorAll('.js-toggleNav');
 
 
-function toggleNav(){
-    navsMobile.forEach(function (nav){
+
+function toggleNav() {
+    navsMobile.forEach(function (nav) {
         nav.classList.toggle('hiddenMobile')
     })
 }
 
-burger.addEventListener('click', () =>{
+burger.addEventListener('click', () => {
     toggleNav();
 })
 
-navsMobile.forEach(nav =>{
-    nav.querySelectorAll('a').forEach((link, index)=>{
-        link.addEventListener('click',()=>{
-        toggleNav();
+navsMobile.forEach(nav => {
+    nav.querySelectorAll('a').forEach((link, index) => {
+        link.addEventListener('click', () => {
+            toggleNav();
         })
     })
 })
 
 
-
-
-
-
-
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    if (scroll > 0) {
+       document.querySelector('header').classList.add('headerScroll')
+    }
+    else {
+        document.querySelector('header').classList.remove('headerScroll')
+    }
+})
